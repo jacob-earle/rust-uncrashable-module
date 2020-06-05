82ea0895363a02a34c7eacb276874a3f26a6703b
Jacob Earle <jacob.earle@yale.edu>
2020-06-05 13:48:26 -0400

    Fixed the issue with undefined symbols by adding a simple kernel module to export the necessary code.

---
+++ src/load_wrmsr.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include "bindings_helper.h"
+
+int init_module(void)
+{
+	printk(KERN_INFO "Loaded my_wrmsr wrapper function.\n");
+
+	/* 
+	 * A non 0 return means init_module failed; module can't be loaded. 
+	 */
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Unloaded my_wrmsr wrapper function.\n");
+}
+
+EXPORT_SYMBOL(my_wrmsr);
+MODULE_LICENSE("GPL");
--- uncrashable_module/load_module.sh
+++ uncrashable_module/load_module.sh
@@ -2,6 +2,16 @@
 #This short script consolidates the commands needed to load the module and mount the character device associated with it
 MODULE=uncrashablemodule
 CHRDEV=/dev/uncrashable
+
+#First we must check whether the load_wrmsr module is already inserted
+if lsmod | grep "load_wrmsr" &> /dev/null ; then
+	echo "The code of load_wrmsr is already loaded into the kernel. Loading the main module now."
+else
+	echo "The code of load_wrmsr is not yet loaded into the kernel. Loading that module first."
+	sudo insmod ../src/load_wrmsr.ko
+	echo "Code successfully inserted. Loading the main module now."
+fi
+
 sudo insmod $MODULE.ko
 sudo mknod $CHRDEV c `grep $MODULE /proc/devices | grep -o [0-9]*` 0 
 sudo chmod a+w $CHRDEV
